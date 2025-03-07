075a7ff98a27532bed70bc719e594a3b68c95e70
Tim_Medv <medvedkotm@mail.ru>
2025-03-07 21:57:29 +0300

    m

--- src/schemas/books.py
+++ src/schemas/books.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel, Field, field_validator
 from pydantic_core import PydanticCustomError
+from typing import Optional
 
 __all__ = ["IncomingBook", "ReturnedBook", "ReturnedAllbooks"]
-
 
 # Базовый класс "Книги", содержащий поля, которые есть во всех классах-наследниках.
 class BaseBook(BaseModel):
@@ -10,27 +10,22 @@ class BaseBook(BaseModel):
     author: str
     year: int
 
-
 # Класс для валидации входящих данных. Не содержит id так как его присваивает БД.
 class IncomingBook(BaseBook):
-    pages: int = Field(
-        default=150, alias="count_pages"
-    )  # Пример использования тонкой настройки полей. Передачи в них метаинформации.
+    pages: int = Field(default=150, alias="count_pages")
+    seller_id: Optional[int] = None
 
-    @field_validator("year")  # Валидатор, проверяет что дата не слишком древняя
+    @field_validator("year")
     @staticmethod
     def validate_year(val: int):
         if val < 2020:
             raise PydanticCustomError("Validation error", "Year is too old!")
-
         return val
-
 
 # Класс, валидирующий исходящие данные. Он уже содержит id
 class ReturnedBook(BaseBook):
     id: int
     pages: int
-
 
 # Класс для возврата массива объектов "Книга"
 class ReturnedAllbooks(BaseModel):
