2c9fac7b0c9764e813b405ee6dd6cc5cc677f90f
Tim_Medv <medvedkotm@mail.ru>
2025-03-07 21:46:20 +0300

    homework

--- src/configurations/settings.py
+++ src/configurations/settings.py
@@ -1,24 +1,28 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), "../../.env")
+load_dotenv(dotenv_path)
 
 class Settings(BaseSettings):
-    db_host: str = "localhost:5432"
-    db_name: str = "postgres"
-    db_username: str = "postgres"
-    db_password: str = "0000"
+    # для PostgreSQL
+    db_host: str
+    db_port: int
+    db_name: str
+    db_username: str
+    db_password: str
     db_test_name: str = "fastapi_project_test_db"
     max_connection_count: int = 10
 
     @property
     def database_url(self) -> str:
-        return f"postgresql+asyncpg://{self.db_username}:{self.db_password}@{self.db_host}/{self.db_name}"
+        return f"postgresql+asyncpg://{self.db_username}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
 
     @property
     def database_test_url(self) -> str:
-        return f"postgresql+asyncpg://{self.db_username}:{self.db_password}@{self.db_host}/{self.db_test_name}"
+        return f"postgresql+asyncpg://{self.db_username}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_test_name}"
 
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-    )
+    model_config = SettingsConfigDict(env_file="../../.env", env_file_encoding="utf-8")
 
 settings = Settings()
--- src/schemas/books.py
+++ src/schemas/books.py
@@ -1,14 +1,16 @@
-from typing import Optional
 from pydantic import BaseModel, Field, field_validator
 from pydantic_core import PydanticCustomError
+from typing import Optional
 
 __all__ = ["IncomingBook", "ReturnedBook", "ReturnedAllbooks"]
 
+# Базовый класс "Книги", содержащий поля, которые есть во всех классах-наследниках.
 class BaseBook(BaseModel):
     title: str
     author: str
     year: int
 
+# Класс для валидации входящих данных. Не содержит id так как его присваивает БД.
 class IncomingBook(BaseBook):
     pages: int = Field(default=150, alias="count_pages")
     seller_id: Optional[int] = None
@@ -20,18 +22,11 @@ class IncomingBook(BaseBook):
             raise PydanticCustomError("Validation error", "Year is too old!")
         return val
 
+# Класс, валидирующий исходящие данные. Он уже содержит id
 class ReturnedBook(BaseBook):
     id: int
     pages: int
-    seller_id: Optional[int] = None
 
-    class Config:
-        orm_mode = True
-        exclude_none = True  # если seller_id равен None, поле не будет включено в ответ
-
+# Класс для возврата массива объектов "Книга"
 class ReturnedAllbooks(BaseModel):
     books: list[ReturnedBook]
-
-    class Config:
-        orm_mode = True
-        exclude_none = True
